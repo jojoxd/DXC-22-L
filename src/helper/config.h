31f40cd13226628da5622e5d036860e5b16f64a4
Joey Vos <contact@jojoxd.nl>
2022-02-12 01:24:24 +0100

    VERSION_STR toegevoegd aan mbed compile

--- src/helper/config.h
+++ src/helper/config.h
@@ -3,4 +3,6 @@
 #define CONSOLE_BAUDRATE 9600
 
 // Niet veranderen, doet build systeem automatisch
-#define VERSION_STR "0.0.0-dev"
+#ifndef VERSION_STR
+    #define VERSION_STR "0.0.0-dev"
+#endif
