4b23fcae12c15a8750ff4cc3c046fb273091561a
Joey Vos <contact@jojoxd.nl>
2022-05-12 01:05:18 +0200

    Add moving average functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,56 @@
 #include <mbed.h>
 
-#include "driver/MotorDriver.h"
-#include "sensor/HCSR04.h"
+//#include "driver/MotorDriver.h"
+//#include "sensor/HCSR04.h"
 #include "helper/macros.h"
-#include "console.hpp"
+//#include "console.hpp"
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
 
 int main()
 {
-    Console& console = Console::getInstance();
+    //Console& console = Console::getInstance();
 
     #ifdef DXC_TEAM_1
         // Team 1 Custom Code
-        console.writeln("Team 1");
+//        console.writeln("Team 1");
     #endif
 
     #ifdef DXC_TEAM_2
         // Team 2 Custom Code
-        console.writeln("Team 2");
+//        console.writeln("Team 2");
     #endif
 
     #ifdef DXC_TEAM_3
         // Team 3 Custom Code
-        console.writeln("Team 3");
+//        console.writeln("Team 3");
     #endif
 
-    HCSR04 sensor(D8, D9);
-    sensor.setRanges(5.0f, 25.0f);
+//    HCSR04 sensor(D6, D7);
+//    sensor.setRanges(5.0f, 25.0f);
+//
+//    InterruptIn btn(D13, PullDown);
+//    MotorDriver driver(D10, D12);
 
-    InterruptIn btn(D13, PullDown);
-    MotorDriver driver(D10, D12);
+//    util::MovingAverage<float, 5, int> avg;
+
+    int test[256] = {0};
+
+//    while(true) {
+//        sensor.startMeasurement();
+//        while(!sensor.isNewDataReady()); // Wait for sensor data
+//
+//        float distanceInCentimeters = sensor.getDistance();
+//        int average = avg.next(distanceInCentimeters);
+//
+//        console.writelnf("Distance: %d cm (avg = %d cm) use test: %d", (int)distanceInCentimeters, average, test[0]);
+//
+//        ThisThread::sleep_for(1000ms);
+//    }
 
     while(true) {
-        console.ISR_handle();
+//        console.ISR_handle();
         ThisThread::sleep_for(250ms);
     }
 
--- src/util.hpp
+++ src/util.hpp
@@ -6,7 +6,7 @@
 namespace util
 {
     template<typename... Args>
-    std::string string_fmt(const std::string &format, Args... args)
+    std::string string_fmt(const std::string& format, Args... args)
     {
         std::size_t size = std::snprintf(nullptr, 0, format.c_str(), args...) + 1;
         auto buf = std::make_unique<char[]>(size);
@@ -14,6 +14,30 @@ namespace util
 
         return std::string(buf.get(), buf.get() + size - 1);
     }
+
+    template<typename T, size_t t_size = 5, typename V = T>
+    struct MovingAverage
+    {
+    public:
+        V next(T sample)
+        {
+            //Subtract the oldest number from the prev sum, add the new number
+            m_sum = m_sum - m_items[m_pos] + sample;
+            //Assign the nextNum to the position in the array
+            m_items[m_pos] = sample;
+
+            // Update next pos
+            m_pos = (m_pos + 1) % t_size;
+
+            //return the average
+            return (V) (m_sum / t_size);
+        }
+
+    protected:
+        T m_items[t_size];
+        T m_sum = 0;
+        int m_pos = 0;
+    };
 
     namespace math
     {
